8824fb08ffde6dde80bfeb7039598006e380e9d2
CrownLis <arkaim.ganibal@mail.ru>
2022-11-29 03:42:51 +0600

    feat: created settings page

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,87 @@
+import MainLayout from "@/layouts/MainLayout";
+import Button from "@/UIComponents/Button";
+import Input from "@/UIComponents/Input";
+import { FC } from "react";
+import styled from "styled-components";
+
+const Settings: FC = () => {
+    return (
+        <MainLayout>
+            <Container>
+                <StyledTitle>Settings</StyledTitle>
+                <FormContainer>
+                    <FormInfo>
+                        <FormTitle>Personal Info</FormTitle>
+                        <StyledInput placeholder="Username" />
+                        <StyledInput placeholder="Email" />
+                        <StyledButton isUnusual>Save all changes</StyledButton>
+                    </FormInfo>
+                    <FormPassword>
+                        <FormTitle>Change password</FormTitle>
+                        <StyledInput placeholder="Current Password" />
+                        <StyledInput placeholder="New Password" />
+                    </FormPassword>
+                </FormContainer>
+            </Container>
+        </MainLayout>
+
+    )
+}
+
+export default Settings
+
+const Container = styled.div`
+    display:flex;
+    flex-direction:column;
+    width:100%;
+    margin:0px 86px;
+    color:white;
+`
+
+const StyledTitle = styled.h3`
+    font-family: 'THICCCBOI';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 54px;
+    line-height: 64px;
+    margin:48px 0;
+`
+
+const FormContainer = styled.form`
+    display:flex;
+    gap:60px;
+`
+
+const FormInfo = styled.div`
+    display:flex;
+    flex-direction:column;
+    gap:24px;
+    width:100%;
+    max-width:512px;
+`
+
+const FormTitle = styled.h3`
+    font-family: 'THICCCBOI';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 40px;
+`
+
+const StyledInput = styled(Input)`
+    max-width:512px;
+`
+
+const StyledButton = styled(Button)`
+    max-width:228px;
+    padding:26px 42px;
+    margin-top:24px;
+    margin-bottom:444px;
+`
+
+const FormPassword = styled.div`
+    display:flex;
+    flex-direction:column;
+    width:100%;
+    gap:24px;   
+`
