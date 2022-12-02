feee5c204c948aadac15f5cc3345429d061ca786
CrownLis <arkaim.ganibal@mail.ru>
2022-12-03 01:52:04 +0600

    feat: create UILoader

---
+++ src/UIComponents/Loader/Loader.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+const Loader:FC = () => {
+    return (
+        <StyledLoader/>
+    )
+}
+
+const StyledLoader = styled(Loader)`
+    animation-duration:800ms;
+    animation-name: rotating;
+    animation-iteration-count:infinite;
+    @keyframes rotating {
+                    0% {
+                        transform:rotate(0deg);
+                    }
+                    25% {
+                        transform:rotate(90deg)
+                    }
+                    50% {
+                        transform:rotate(180deg);
+                    }
+                    75% {
+                        transform:rotate(270deg);
+                    }
+                    100% {
+                        transform:rotate(360deg);
+                    }
+                }
+`
