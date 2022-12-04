3a842652e1448552c2bb539cb922a9fd22d6c9fd
CrownLis <arkaim.ganibal@mail.ru>
2022-12-04 23:39:02 +0600

    refactor: move utils in folder

--- src/utils/hex2rgba.ts
+++ src/utils/hex2rgba.ts
@@ -1,4 +1,6 @@
-export const hex2rgba = (hex: string, alpha = 1) => {
+const hex2rgba = (hex: string, alpha = 1) => {
     const [r, g, b] = hex.match(/\w\w/g)!.map((x: string) => parseInt(x, 16));
     return `rgba(${r},${g},${b},${alpha})`;
 };
+
+export default hex2rgba
